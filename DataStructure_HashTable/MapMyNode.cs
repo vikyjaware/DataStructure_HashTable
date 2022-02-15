1db3bceab9f46d0024377cd63a7428183935f062
vikyjaware <vikyjaware@gmail.com>
2022-02-16 00:39:41 +0530

    [Viky] Add : Ability to find frequency Of word in a Sentence added

---
+++ DataStructure_HashTable/MapMyNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_HashTable
+{
+    public struct KeyValue<k, v>
+    {
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+
+    public class MyMapNode<k, v>
+    {
+        public int size;
+        public LinkedList<KeyValue<k, v>>[] element;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.element = new LinkedList<KeyValue<k, v>>[size];
+        }
+
+        public LinkedList<KeyValue<k, v>> GetLinkedListElement(int position)
+        {
+            LinkedList<KeyValue<k, v>> list = element[position];
+            if (list == null)
+            {
+                list = new LinkedList<KeyValue<k, v>>();
+                element[position] = list;
+            }
+            return list;
+        }
+
+        public int GetArrayPosition(k key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+        public v GetElement(k key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<k, v>> list = GetLinkedListElement(position);
+
+            foreach (KeyValue<k, v> item in list)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(v);
+        }
+
+        public void Add(k key, v value)
+        {
+            int state = GetArrayPosition(key);
+            LinkedList<KeyValue<k, v>> list = GetLinkedListElement(state);
+            KeyValue<k, v> element = new KeyValue<k, v>() { Key = key, Value = value };
+            list.AddLast(element);
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ DataStructure_HashTable/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructure_HashTable;
+
+MyMapNode<string, string> hashTable = new MyMapNode<string, string>(5);
+
+hashTable.Add("0", "To");
+hashTable.Add("1", "be");
+hashTable.Add("2", "or");
+hashTable.Add("3", "not");
+hashTable.Add("4", "to");
+hashTable.Add("5", "be");
+
+string strHold = hashTable.GetElement("0");
+System.Console.WriteLine("4th index value is : " + strHold);
